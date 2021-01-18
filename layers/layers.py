e54fa7aa1fa87f165969dfa45e25b66b6aa2b29e
Milosz Grabski <m.grabski@sygnaturediscovery.com>
2021-01-18 16:24:06 +0000

    layers, featurizer update
    Added get_config() method to custom layers. Updated featurizer to be in-line with DeepChem featurizer.

--- layers/layers.py
+++ layers/layers.py
@@ -2,19 +2,22 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 class GraphConvolutionLayer(layers.Layer):
-    def __init__(self, unit, activation=None, dropout_rate=0., edges=5, name='', **kwargs):
+    def __init__(self, units, activation=None, dropout_rate=0., edges=5, name='', **kwargs):
         super(GraphConvolutionLayer, self).__init__(name=name, **kwargs)
         self.activation = activation
         self.dropout_rate = dropout_rate
-        self.dense1 = [layers.Dense(units=unit) for _ in range(edges-1)]
-        self.dense2 = layers.Dense(units=unit)
-        self.dropout = layers.Dropout(dropout_rate)
-        self.activation = layers.Activation(activation)
+        self.unit = units
+        self.edges = edges
+        
+        self.dense1 = [layers.Dense(units=self.units) for _ in range(edges-1)]
+        self.dense2 = layers.Dense(units=self.units)
+        self.dropout = layers.Dropout(self.dropout_rate)
+        self.activation = layers.Activation(self.activation)
         
     def call(self, inputs, training=False):
         ic = len(inputs)
         if ic<2:
-            raise ValueError('GraphConvolutionLayer requires at leat two inputs: [adjacency_tensor, node_tensor]')
+            raise ValueError('GraphConvolutionLayer requires at leat two inputs: [adjacency_tensor, node_features_tensor]')
             
         adjacency_tensor = inputs[0]
         node_tensor = inputs[1]
@@ -35,32 +38,58 @@ class GraphConvolutionLayer(layers.Layer):
         output = self.activation(output)
         output = self.dropout(output)
         return adjacency_tensor, node_tensor, output
+    
+      def get_config(self) -> Dict:
+        """Returns config dictionary for this layer."""
+        config = super(GraphConvolutionLayer, self).get_config()
+        config['activation'] = self.activation
+        config['dropout_rate'] = self.dropout_rate
+        config['units'] = self.units
+        config['edges'] = self.edges
+        return config
 
 class GraphAggregationLayer(layers.Layer):
     def __init__(self, units, activation=None, dropout_rate=0., name='', **kwargs):
         super(GraphAggregationLayer, self).__init__(name=name, **kwargs)
+        self.units = units
+        self.activation = activation
+        self.dropout_rate = dropout_rate
+        
         self.d1 = layers.Dense(units = units, activation='sigmoid')
         self.d2 = layers.Dense(units = units, activation=activation)
-        self.dropout = layers.Dropout(dropout_rate)
-        self.activation = layers.Activation(activation)
+        self.dropout_layer = layers.Dropout(dropout_rate)
+        self.activation_layer = layers.Activation(activation)
         
     def call(self, inputs, training=False):
         i = self.d1(inputs)
         j = self.d2(inputs)
         output = tf.reduce_sum(i*j,1)
-        output = self.activation(output)
-        output = self.dropout(output)
+        output = self.activation_layer(output)
+        output = self.dropout_layer(output)
         return output
+    
+    def get_config(self) -> Dict:
+        """Returns config dictionary for this layer."""
+        config = super(GraphAggregationLayer, self).get_config()
+        config['units'] = self.units
+        config['activation'] = self.activation
+        config['dropout_rate'] = self.dropout_rate
+        config['edges'] = self.edges
+        return config
     
 class MultiGraphConvolutionLayer(layers.Layer):
     def __init__(self, units, activation=None, dropout_rate = 0., edges=5, name='', **kwargs):
         super(MultiGraphConvolutionLayer, self).__init__(name=name, **kwargs)
-        
         if len(units) < 2:
             raise ValueError('Single layer unit provided, this layer is for multiple convolutions only. Use GraphConvolutionLayer instead.') 
+                
+        self.units = units
+        self.activation = activation
+        self.dropout_rate = dropout_rate
+        self.edges = edges
         
-        self.first_convolution =  GraphConvolutionLayer(units[0], activation, dropout_rate, edges)
-        self.gcl = [GraphConvolutionLayer(u, activation, dropout_rate, edges) for u in units[1:]]
+        self.first_convolution =  GraphConvolutionLayer(self.units[0], self.activation, self.dropout_rate, self.edges)
+        self.gcl = [GraphConvolutionLayer(u, self.activation, self.dropout_rate, self.edges) for u in self.units[1:]]
 
         
     def call(self, inputs, training=False):
@@ -76,15 +105,27 @@ class MultiGraphConvolutionLayer(layers.Layer):
             
         return hidden_tensor
     
+    def get_config(self) -> Dict:
+        """Returns config dictionary for this layer."""
+        config = super(MultiGraphConvolutionLayer, self).get_config()
+        config['units'] = self.units
+        config['activation'] = self.activation
+        config['dropout_rate'] = self.dropout_rate
+        config['edges'] = self.edges
+        return config
+    
 class GraphEncoderLayer(layers.Layer):
     def __init__(self, units, activation='tanh', dropout_rate=0., edges= 5, name='', **kwargs):
         super(GraphEncoderLayer, self).__init__(name=name, **kwargs)
-        graph_convolution_units, auxiliary_units = units
+        self.graph_convolution_units, self.auxiliary_units = units
+        self.activation = activation
+        self.dropout_rate = dropout_rate
+        self.edges = edges
         
-        self.multi_graph_convolution_layer =  MultiGraphConvolutionLayer(graph_convolution_units, activation, dropout_rate, edges)
-        self.graph_aggregation_layer = GraphAggregationLayer(auxiliary_units, activation, dropout_rate)
+        self.multi_graph_convolution_layer =  MultiGraphConvolutionLayer(self.graph_convolution_units, self.activation, self.dropout_rate, self.edges)
+        self.graph_aggregation_layer = GraphAggregationLayer(self.auxiliary_units, self.activation, self.dropout_rate)
         
-    def call(self, inputs, training=False, test=True):
+    def call(self, inputs, training=False):
         
         output = self.multi_graph_convolution_layer(inputs)
         
@@ -100,4 +141,14 @@ class GraphEncoderLayer(layers.Layer):
         
         output = self.graph_aggregation_layer(annotations)
         return output
+    
+    def get_config(self) -> Dict:
+        """Returns config dictionary for this layer."""
+        config = super(GraphEncoderLayer, self).get_config()
+        config['graph_convolution_units'] = self.graph_convolution_units
+        config['auxiliary_units'] = self.auxiliary_units
+        config['activation'] = self.activation
+        config['dropout_rate'] = self.dropout_rate
+        config['edges'] = self.edges
+        return config
     
